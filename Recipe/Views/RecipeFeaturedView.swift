33d793f722c945a2b0d47d03aa2fec855e310d11
Mateusz <matikg@interia.eu>
2024-03-23 09:22:39 +0100

    - Add RecipeFeaturedView - Modify data flow using environment object modifier

---
+++ Recipe/Views/RecipeFeaturedView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe
+//
+//  Created by Mateusz Grudzień on 23/03/2024.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeFeaturedView()
+}
