9bd977009b326b008018143eb156896d726c7335
Mateusz <matikg@interia.eu>
2024-03-27 16:11:51 +0100

    Update text info in FeaturedView

---
+++ Recipe/Views/RecipeHighlights.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeHighlights.swift
+//  Recipe
+//
+//  Created by Mateusz Grudzień on 27/03/2024.
+//
+
+import SwiftUI
+
+struct RecipeHighlights: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeHighlights()
+}
