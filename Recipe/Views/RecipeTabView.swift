78d42dd99c20b13b210667346dab45b1eba58525
Mateusz <matikg@interia.eu>
2024-03-27 16:20:38 +0100

    Test

--- Recipe/Views/RecipeTabView.swift
+++ Recipe/Views/RecipeTabView.swift
@@ -11,7 +11,7 @@ struct RecipeTabView: View {
     var body: some View {
         
         TabView {
-            
+             
             RecipeFeaturedView()
                 .tabItem {
                     VStack {
